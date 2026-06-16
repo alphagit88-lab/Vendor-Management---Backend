247e21a5a70f2aed27d3312dd89d613e1b452a6f
alphagit88 <alphagit88@gmail.com>
2026-06-16 22:34:06 +0530

    email

--- utils/emailService.js
+++ utils/emailService.js
@@ -68,7 +68,7 @@ const sendInvoiceEdiEmail = async ({
 
   await transporter.sendMail({
     from: `"${shop.company_name || 'SuperVendor'}" <${process.env.AWS_SES_FROM_EMAIL}>`,
-    to: "shanmadusanka969@gmail.com",
+    to: "chathura.itexphere@gmail.com",
     subject: `Invoice #${orderNumber} – EDI File Attached`,
     html,
     attachments: [
